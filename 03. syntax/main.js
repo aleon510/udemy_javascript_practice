2dfa82c3ca25d4f0ba7b2c02fd6fbfc1fe8f6f0b
Alex Leon <alejandroleon5434@gmail.com>
2021-03-06 19:59:06 -0800

    syntax, statements, variables, operators explained for JavaScript

---
+++ 03. syntax/main.js
@@ -0,0 +1,58 @@
+var x, y, z; // How to declare Variables
+x = 5;
+y = 6; // How to assign values
+z = x + y; // How to compute values
+
+// Fixed values are called literals. Values are called Variables
+
+// JavaScript Literals
+
+10.5;
+1001;
+('John Doe');
+
+('John Doe');
+
+// JavaScript Variables
+
+var x;
+x = 6;
+
+console.log((x = 6));
+
+// JavaScript Operators
+(5 + 6) * 50;
+
+var x, z;
+x = 10;
+z = 100;
+
+10 * 20;
+
+x * 10;
+
+var z, a;
+z = 5 + 8;
+a = 8 * 10 * x;
+
+// JavaSciript is Case Senstive
+
+var lastname, LastName;
+lastname = 'Bob';
+lastName = 'Jones';
+// JavaScript prefers to use you Camel case
+firstName = '';
+userLoginCount = '';
+
+//Pascal case
+UserLoginCount = '';
+
+// Snake Case (snake_case)
+
+user_login_count = '';
+// or
+USER_LOGIN_COUNT = '';
+
+// Kebab Case (kebab-case)
+
+user - login - count;
---
+++ 04. statements/main.js
@@ -0,0 +1,9 @@
+document.getElementById('test').innerHTML = 'Hello World';
+
+var a, b, c; // Declare 3 Variables
+a = 10; // Assign the value 10 to a
+b = 60; // Assign the value 60 to b
+c = a + b; // Assign the sum of a and b to c
+
+// Assigning values onto on single line
+a = 5; b = 6; c = a + b;
